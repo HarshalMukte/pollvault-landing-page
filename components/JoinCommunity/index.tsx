10af484451a80ee84f4b516232431e6768a9a9d1
Harshal <harshalmukte@gmail.com>
2024-10-24 21:46:15 +0530

    updates

--- components/JoinCommunity/index.tsx
+++ components/JoinCommunity/index.tsx
@@ -5,54 +5,66 @@ import Image from "@/components/Image";
 import Animation from "@/components/Animation";
 
 import { avatars, images } from "@/constants/joinCommunity";
+import Form from "../Form";
+import { useState } from "react";
 
 type JoinCommunityProps = {};
 
-const JoinCommunity = ({}: JoinCommunityProps) => (
-    <div className={styles.join}>
+const JoinCommunity = ({}: JoinCommunityProps) => {
+    const [email, setEmail] = useState("")
+
+    return (
+        <div className={styles.join}>
         <div className={cn("container", styles.container)}>
-            <div className={styles.wrap}>
-                <div className={cn("h1", styles.title)}>
-                Ready to WOW your audience?
-                </div>
-                <div className={cn("h4M", styles.info)}>
-                Join the waitlist for the exclusive Pollvault community.
-                </div>
-                <div className={styles.avatars}>
-                    {avatars.map((avatar, index) => (
-                        <div className={styles.avatar} key={index}>
-                            <Image
-                                src={avatar}
-                                layout="fill"
-                                alt="Avatar"
-                                priority
-                            />
-                        </div>
-                    ))}
-                </div>
-                <Link href="/pricing">
-                    <a className={cn("button", styles.button)}>Get started</a>
-                </Link>
+          <div className={styles.wrap}>
+            <div className={cn("h1", styles.title)}>
+              Ready to WOW your audience?
             </div>
-            <div className={styles.images}>
-                {images.map((image, index) => (
-                    <Animation
-                        className={styles.image}
-                        animateIn="fadeIn"
-                        key={index}
-                        speed={index < 4 ? -5 : 0}
-                    >
-                        <Image
-                            src={image.src}
-                            width={image.width}
-                            height={image.height}
-                            alt={image.alt}
-                        />
-                    </Animation>
-                ))}
+            <div className={cn("h4M", styles.info)}>
+              Join the waitlist for the exclusive Pollvault community.
             </div>
+            <div className={styles.avatars}>
+              {avatars.map((avatar, index) => (
+                <div className={styles.avatar} key={index}>
+                  <Image src={avatar} layout="fill" alt="Avatar" priority />
+                </div>
+              ))}
+            </div>
+            <div className={styles.formContainer}>
+              <Form
+                className={styles.form}
+                placeholder="Join the waitlist"
+                type="email"
+                icon="mail"
+                value={email}
+                setValue={setEmail}
+                onSubmit={() => console.log("Submit")}
+              />
+            </div>
+            {/* <Link href="/pricing">
+              <a className={cn("button", styles.button)}>Get started</a>
+            </Link> */}
+          </div>
+          <div className={styles.images}>
+            {images.map((image, index) => (
+              <Animation
+                className={styles.image}
+                animateIn="fadeIn"
+                key={index}
+                speed={index < 4 ? -5 : 0}
+              >
+                <Image
+                  src={image.src}
+                  width={image.width}
+                  height={image.height}
+                  alt={image.alt}
+                />
+              </Animation>
+            ))}
+          </div>
         </div>
-    </div>
-);
+      </div>
+    )
+};
 
 export default JoinCommunity;
